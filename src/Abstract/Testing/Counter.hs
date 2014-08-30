f572cf553b4b81d7e0a2f5eb4214c48e0b19f452
Andrew Darqui <andrew.darqui@gmail.com>
2014-08-30 13:00:29 -0400

    testing

--- src/Abstract/Testing/Counter.hs
+++ src/Abstract/Testing/Counter.hs
@@ -10,6 +10,11 @@ import Abstract.Impl.Libs.Counter
   mkCounter'IORef, mkCounter'IORef'Inc, mkCounter'IORef'Dec, mkCounter'IORef'Get
  )
 
+import Abstract.Impl.Redis.Counter
+ (
+  mkCounter'Redis'Int, mkCounter'Redis'Int'Inc, mkCounter'Redis'Int'Dec, mkCounter'Redis'Int'Get, defaultCounterRedisWrapper'Int
+ )
+
 import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Monad
@@ -17,23 +22,53 @@ import Control.Monad
 -- hideous
 
 runCounterTests threads maxN = do
- mvar <- mkCounter'MVar "mvar counter" 0
- _ <- runCounterTests' mvar threads maxN
- ior <- mkCounter'IORef "ioref counter" 0
- _ <- runCounterTests' ior threads maxN
- red <- mkCounter'IORef "redis counter" 0
- _ <- runCounterTests' red threads maxN
+ mvar <- mkCounter'MVar 0
+ _ <- runCounterTests' "mvar" mvar threads maxN
+ ior <- mkCounter'IORef 0
+ _ <- runCounterTests' "ioref" ior threads maxN
+ red <- mkCounter'Redis'Int 0 (defaultCounterRedisWrapper'Int "counter")
+ _ <- runCounterTests' "redis" red threads maxN
  return ()
 
-runCounterTests' ctr threads maxN = do
+runCounterTests' s ctr threads maxN = do
+ putStrLn $ "test: " ++ s
  _ <- reset ctr
- a1 <- async (forM_ [1..threads] (\_ -> runCounterTest ctr maxN))
+ test'incr ctr threads maxN
+ _ <- reset ctr
+ test'incrBy ctr threads maxN
+ test'decr ctr threads maxN
+ test'gentleReset ctr
+ return ()
+
+test'incr ctr threads maxN = do
+ a1 <- async (forM_ [1..threads] (\_ -> forM_ [1..maxN] $ \_ -> incr ctr))
  w <- wait a1
  (Just result) <- get ctr
  case (result == (threads * maxN)) of
-  True -> putStrLn "success" >> return ()
-  False -> error $ "Fail: " ++ (show (threads * maxN)) ++ " /= " ++ show result
+  True -> putStrLn "test'incr: success" >> return ()
+  False -> error $ "test'incr: fail: " ++ (show (threads * maxN)) ++ " /= " ++ show result
 
-runCounterTest ctr n = do
- forM_ [1..n] $ \_ -> incr ctr
- return ()
+test'incrBy ctr threads maxN = do
+ a1 <- async (forM_ [1..threads] (\_ -> incrBy ctr maxN >> return ()))
+ w <- wait a1
+ (Just result) <- get ctr
+ case (result == (threads * maxN)) of
+  True -> putStrLn "test'incrBy: success" >> return ()
+  False -> error $ "test'incrBy: fail: " ++ (show (threads * maxN)) ++ " /= " ++ show result
+
+test'decr ctr threads maxN = do
+ a1 <- async (forM_ [1..threads] (\_ -> forM_ [1..maxN] $ \_ -> decr ctr))
+ w <- wait a1
+ (Just result) <- get ctr
+ case (result == 0) of
+  True -> putStrLn "test'decr: success" >> return ()
+  False -> error $ "test'decr: fail: " ++ (show (threads * maxN)) ++ " /= " ++ show result
+
+test'gentleReset ctr = do
+ v <- get ctr
+ _ <- gentleReset ctr
+ v' <- get ctr
+ putStrLn $ show v ++ " " ++ show v'
+ case (v == v') of
+  True -> putStrLn "test'gentleReset: success" >> return ()
+  False -> error $ "test'gentleReset: fail"
